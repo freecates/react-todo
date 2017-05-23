07336ca1c9dcd29012d45048cc1c79721768db7a
freecates <ramongil@ramongil.cat>
2017-05-23 12:53:22 +0200

    initial TodoApp

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+var TodoApp = React.createClass({
+    render: function () {
+        return (
+            <div>
+                TodoApp.jsx
+            </div>
+        )
+    }
+
+});
+
+module.exports = TodoApp;
